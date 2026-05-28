3125600f34dfee8e081f02bead50c3921eb38e02
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-05-28 12:36:36 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -8,9 +8,20 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-
     int sum = 0;
+    int num;
+    char buffer[256];
 
+    while (1) {
+        if (fscanf(fp, "%d", &num) == 1) {
+            sum += num;
+        } else {
+            if (fscanf(fp, "%255s", buffer) == EOF) {
+                break;
+            }
+            fprintf(stderr, "invalid input %s\n", buffer);
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
